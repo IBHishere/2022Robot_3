c4540e90b717426a75eb9db8d7d7381502134654
Sam <Srabbjaros@gmail.com>
2022-02-19 10:55:22 -0500

    dont use

--- 2022Drive/src/main/java/frc/robot/RobotContainer.java
+++ 2022Drive/src/main/java/frc/robot/RobotContainer.java
@@ -9,9 +9,10 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
-import frc.robot.commands.AutoCommand;
-import frc.robot.commands.ShootCommands;
-import frc.robot.commands.TankDriveCommand;
+//import frc.robot.commands.AutoCommand;
+//import frc.robot.commands.PIDTurnRobotCommand;
+//import frc.robot.commands.ShootCommands;
+//import frc.robot.commands.TankDriveCommand;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -38,10 +39,10 @@ public class RobotContainer {
   private final DriveTrainSubsystem m_tankDriveSubsystem = new DriveTrainSubsystem();
   private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
    private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-   AutoCommand m_autoCommand = new AutoCommand(
-                this.m_visionSubsystem, this.m_tankDriveSubsystem, this.m_shooterSubsystem, this.m_intakeSubsystem);
-  ShootCommands m_shootCommand = new ShootCommands(
-                this.m_shooterSubsystem);
+  //  AutoCommand m_autoCommand = new AutoCommand(
+  //               this.m_visionSubsystem, this.m_tankDriveSubsystem, this.m_shooterSubsystem, this.m_intakeSubsystem);
+  // ShootCommands m_shootCommand = new ShootCommands(
+  //               this.m_shooterSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -51,10 +52,10 @@ public class RobotContainer {
    //         this.m_intakeSubsystem);
    //   this.m_intakeSubsystem.setDefaultCommand(m_intakecommand);
     
-    TankDriveCommand command = new TankDriveCommand(
-               this.m_tankDriveSubsystem, m_driveController::getLeftY, m_driveController::getRightY);
-    this.m_tankDriveSubsystem.setDefaultCommand(command);
- this.m_shooterSubsystem.setDefaultCommand(m_shootCommand);
+    // TankDriveCommand command = new TankDriveCommand(
+    //            this.m_tankDriveSubsystem, m_driveController::getLeftY, m_driveController::getRightY);
+    // this.m_tankDriveSubsystem.setDefaultCommand(command);
+//  this.m_shooterSubsystem.setDefaultCommand(m_shootCommand);
 
   }
 
@@ -68,13 +69,15 @@ public class RobotContainer {
 
     table.getEntry("isXboxConnected").forceSetBoolean( m_helperController.isConnected() );
     
-    new JoystickButton (m_helperController, Button.kA.value)
-    .whenPressed( 
-      ()-> {
-        table.getEntry("test").forceSetString("A-button pressed");
-        this.m_visionSubsystem.ToggleCameraState();
-      }
-    );
+    // new JoystickButton (m_helperController, Button.kA.value)
+    // .whenPressed( 
+    //   ()-> {
+    //     table.getEntry("test").forceSetString("A-button pressed");
+    //     this.m_visionSubsystem.ToggleCameraState();
+    //   }
+    // );
+    // new JoystickButton(m_helperContrPoller, Button.kA.value)
+    // .whenPressed(new PIDTurnRobotCommand(this.m_tankDriveSubsystem, 10));
 
     new JoystickButton (m_helperController, Button.kRightBumper.value)
     .whenPressed( 
@@ -83,6 +86,7 @@ public class RobotContainer {
         this.m_intakeSubsystem.intakePull();
       }
     );
+
     new JoystickButton (m_helperController, Button.kLeftBumper.value)
     .whenPressed( 
       ()-> {
@@ -155,8 +159,8 @@ new JoystickButton(m_driveController, Button.kX.value)
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+  //public Command getAutonomousCommand() {
     
-    return this.m_autoCommand;
-  }
+    //return this.m_autoCommand;
+  //}
 }
