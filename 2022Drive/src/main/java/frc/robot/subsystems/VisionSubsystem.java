a890877d0e3e94133e1ce05efb6be0c629faccec
IBHishere <jais24@parkschool.net>
2022-02-23 19:20:14 -0500

    stuff

--- 2022Drive/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ 2022Drive/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,45 +1,45 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+// package frc.robot.subsystems;
 
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.cameraserver.CameraServer;
+// import edu.wpi.first.cscore.UsbCamera;
+// import edu.wpi.first.networktables.NetworkTable;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class VisionSubsystem extends SubsystemBase {
-  /** Creates a new VisionSubsystem. */
-  private UsbCamera m_usbCamera;
-  private NetworkTableInstance inst = NetworkTableInstance.getDefault();
-  private NetworkTable table = inst.getTable("visionSubsytem");
+// public class VisionSubsystem extends SubsystemBase {
+//   /** Creates a new VisionSubsystem. */
+//   private UsbCamera m_usbCamera;
+//   private NetworkTableInstance inst = NetworkTableInstance.getDefault();
+//   private NetworkTable table = inst.getTable("visionSubsytem");
     
   
-  public VisionSubsystem() {
-    table.getEntry("lastLog").forceSetString("init");
+//   public VisionSubsystem() {
+//     table.getEntry("lastLog").forceSetString("init");
     
-    table.getEntry("lastLog").forceSetString("init2");
-  }
+//     table.getEntry("lastLog").forceSetString("init2");
+//   }
 
-  public void ToggleCameraState() {
-    table.getEntry("lastLog").forceSetString("Camera on");
-    if (m_usbCamera == null ) {
-      m_usbCamera = CameraServer.startAutomaticCapture();
-      table.getEntry("lastLog").forceSetString("Camera on");
-    }
-    else {
-      m_usbCamera.close();
-      m_usbCamera = null;
+//   public void ToggleCameraState() {
+//     table.getEntry("lastLog").forceSetString("Camera on");
+//     if (m_usbCamera == null ) {
+//       m_usbCamera = CameraServer.startAutomaticCapture();
+//       table.getEntry("lastLog").forceSetString("Camera on");
+//     }
+//     else {
+//       m_usbCamera.close();
+//       m_usbCamera = null;
       
-      table.getEntry("lastLog").forceSetString("Camera off");
-    }
-  }
+//       table.getEntry("lastLog").forceSetString("Camera off");
+//     }
+//   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-}
+//   @Override
+//   public void periodic() {
+//     // This method will be called once per scheduler run
+//   }
+// }
