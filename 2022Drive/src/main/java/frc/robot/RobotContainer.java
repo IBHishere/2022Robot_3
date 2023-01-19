b09eb2a5fccf8298853b874bce51f6fd38307dd2
Sam <Srabbjaros@gmail.com>
2023-01-19 15:31:23 -0500

    deleted commented out things

--- 2022Drive/src/main/java/frc/robot/RobotContainer.java
+++ 2022Drive/src/main/java/frc/robot/RobotContainer.java
@@ -45,26 +45,7 @@ XboxController  m_driveController = new XboxController(Constants.DRIVE_XBOX_CONT
   
   // The robot's subsystems and commands are defined here...
   private final limelight test = new limelight();
-  // private final ClimberSubsystem m_leftClimberSubsystem = new ClimberSubsystem("left", Constants.CLIMBER_MOTOR_CANLEFT_ID, true);
-  // private final ClimberSubsystem m_rightClimberSubsystem = new ClimberSubsystem("right", Constants.CLIMBER_MOTOR_CANRIGHT_ID, false);
-  // private final DriveTrainSubsystem m_tankDriveSubsystem = new DriveTrainSubsystem();
-  // private final BeltSubsystem m_beltSubsystem = new BeltSubsystem();
-  // private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem( this.m_beltSubsystem);
-  // private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem(this.m_beltSubsystem);
-  // private final LimelightVisionSubsystem m_limelightVisionSubsystem = new LimelightVisionSubsystem();
-  // //private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
-  // private final QueueFeederWheelSubsystem m_queueFeederWheelSubsystem = new QueueFeederWheelSubsystem();
-  // ShootCommands m_shootCommand = new ShootCommands(
-  //               this.m_shooterSubsystem);
-
-  // AutonSequentialCommands m_autonomous = new AutonSequentialCommands(
-  //   this.m_tankDriveSubsystem
-  //   , this.m_intakeSubsystem
-  //   , this.m_shooterSubsystem
-  //   , this.m_limelightVisionSubsystem
-  //   , this.m_beltSubsystem
-  //   , this.m_queueFeederWheelSubsystem
-  //   );
+  
   private edu.wpi.first.wpilibj2.command.button.Button whenPressed;
  // PIDTurnRobotCommand m_PIDTurnRobotCommand = new PIDTurnRobotCommand(this.m_tankDriveSubsystem, targetAngle);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -75,28 +56,7 @@ XboxController  m_driveController = new XboxController(Constants.DRIVE_XBOX_CONT
     
     configureButtonBindings();
     
-    // TankDriveCommand command = new TankDriveCommand(
-    //     this.m_tankDriveSubsystem, 
-    //     m_driveController::getLeftY, 
-    //     m_driveController::getRightY);
-    
-    // this.m_tankDriveSubsystem.setDefaultCommand(command);
-    // this.m_rightClimberSubsystem.setDefaultCommand(
-    //   new WindClimberCommand(
-    //       this.m_rightClimberSubsystem, 
-    //       this.m_helperController::getRightY)
-    // );
 
-    // this.m_leftClimberSubsystem.setDefaultCommand(
-    //   new WindClimberCommand(
-    //       this.m_leftClimberSubsystem,  
-    //       this.m_helperController::getLeftY)
-    // );
- 
-
-
-//    this.m_shooterSubsystem.setDefaultCommand(m_shootCommand);
-    
   }
   
 
@@ -110,105 +70,12 @@ XboxController  m_driveController = new XboxController(Constants.DRIVE_XBOX_CONT
 
     table.getEntry("isXboxConnected").forceSetBoolean( m_helperController.isConnected() );
     
-    // new JoystickButton (this.m_driveController, Button.kRightBumper.value)
-    // .whenPressed( ()-> this.m_tankDriveSubsystem.setSpeed(DriveTrainSubsystem.highSpeedLimit));
-
-    // new JoystickButton (this.m_driveController, Button.kLeftBumper.value)
-    // .whenPressed( ()-> this.m_tankDriveSubsystem.setSpeed(DriveTrainSubsystem.lowSpeedLimit));
-    
-
-    //TODO: decide if we are doing anything with vision
-    
-    // new JoystickButton (m_helperController, Button.kA.value)
-    // .whenPressed( 
-    //   ()-> {
-    //     table.getEntry("test").forceSetString("A-button pressed");
-    //     this.m_visionSubsystem.ToggleCameraState();
-    //   }
-    // );
 
     new JoystickButton(m_helperController, Button.kB.value)
     .whenPressed(()->{
         limelight.test();
     }
      ); 
-    
-
-
-    //Start: Intake controls
-    // new JoystickButton (m_helperController, Button.kY.value)
-    // .whenPressed( new InstantCommand(
-    //   ()-> {
-    //     table.getEntry("intake").forceSetString("Rt-Bumper/helper pressed: intakePull");
-    //     this.m_intakeSubsystem.intakePull();
-    //   }, this.m_intakeSubsystem, this.m_beltSubsystem )
-    // );
-
-    // new JoystickButton (m_helperController, Button.kA.value)
-    // .whenPressed( new InstantCommand( 
-    //   ()-> {
-    //     table.getEntry("intake").forceSetString("Lt-Bumper/helper pressed: intakePush");
-    //     this.m_intakeSubsystem.intakePush();
-    //   }, this.m_intakeSubsystem, this.m_beltSubsystem)
-    // );
-
-    // new JoystickButton(m_helperController, Button.kB.value)
-    // .whenPressed( new InstantCommand( 
-    //   ()-> {
-    //     table.getEntry("intake").forceSetString("B-Button/drive pressed: intakeStop");
-    //     this.m_intakeSubsystem.intakeStop();
-    //   } , this.m_intakeSubsystem, this.m_beltSubsystem)
-    // );
-    // //End: intake controls
-
-    // //Shooter controls
-    // new JoystickButton(m_helperController, Button.kX.value)
-    // .whenPressed(
-    //   //new ShooterTestCommand(this.m_shooterSubsystem)
-    
-    //   new ShootSequence(this.m_shooterSubsystem, this.m_beltSubsystem, this.m_queueFeederWheelSubsystem)
-    // );
-
-    // new JoystickButton(m_driveController, Button.kX.value)
-    // .whenPressed(
-      
-    //   // new FollowLimelightSequence(this.m_tankDriveSubsystem, this.m_limelightVisionSubsystem)
-    // );
-
-    // new JoystickButton(m_helperController, Button.kRightBumper.value)
-    // .whenPressed( 
-    //   climberMoveCommand(Constants.CLIMBDISTANCE, 500, true, getExtendClimberPidSettings()) // fast going up
-    // );
-
-    
-    // new JoystickButton(m_helperController, Button.kLeftBumper.value)
-    // .whenPressed(
-    //   climberMoveCommand(0, 5000, false, getLiftPidSettings()) //slow going down as robot is pulling up
-    // );
-
-
-    // new JoystickButton(m_helperController, Button.kRightStick.value)
-    // .whenPressed(
-    //   zeroClimber(this.m_rightClimberSubsystem, -20)
-    // );
-
-    // new JoystickButton(m_helperController, Button.kLeftStick.value)
-    //   .whenPressed(
-    //     zeroClimber(this.m_leftClimberSubsystem, -20)
-    // );
-
-    // new JoystickButton(m_helperController, Button.kStart.value)
-    //   .whenPressed(()->
-    //    {
-    //      table.getEntry("tetsts").setString("asdasdasd");
-    //       ShooterSubsystem.ToggleGoalMode();
-    //       System.out.println("tring to toggle goale mode");}
-
-
-
-      
-    
-    //End: Queue controls
     
   }
 
@@ -224,63 +91,4 @@ XboxController  m_driveController = new XboxController(Constants.DRIVE_XBOX_CONT
   }
 
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * //@return the command to run in autonomous
-   */
-  // public Command getAutonomousCommand() {
-  //   this.table.getEntry("autonomousStarted").setBoolean(true);
-    //return m_autonomous;
-    // return new AutonSequentialCommands(
-    //   this.m_tankDriveSubsystem
-    //   , this.m_intakeSubsystem
-    //   , this.m_shooterSubsystem
-    //   , this.m_limelightVisionSubsystem
-    //   , this.m_beltSubsystem
-    //   , this.m_queueFeederWheelSubsystem
-    //   );
-  
-
-  // private SequentialCommandGroup climberMoveCommand(
-  //         double positionTarget
-  //       , double moveTimeInMilliseconds
-  //       , boolean doesItEverEnd
-  //       , PidSettings pidSettings) {
-  //   return new InstantCommand( ()->{System.out.println("StartClimb, " + new Date().getTime());} )
-  //   .andThen(
-  //     new ParallelCommandGroup(
-
-  //       new PIDClimbCommand(
-  //         m_rightClimberSubsystem,                //subsystem
-  //         new LinearSetpointTrajectory(
-  //           m_rightClimberSubsystem.getPosition(), 
-  //           positionTarget, moveTimeInMilliseconds, "rightClimber"),
-  //         1,                                     // climb speed
-  //         doesItEverEnd,                                   // does it end (otherwise keep holding)
-  //         "right",
-  //         pidSettings)
-  //        ,
-  //       new PIDClimbCommand( 
-  //         m_leftClimberSubsystem,
-  //         new LinearSetpointTrajectory(
-  //           m_leftClimberSubsystem.getPosition(), 
-  //           positionTarget, moveTimeInMilliseconds, "leftClimber"),
-  //       1,                                     // climb speed
-  //       doesItEverEnd,                                   // does it end (otherwise keep holding)
-  //       "left",
-  //       pidSettings)
-  //     ));
-  // }
-
-  // private Command zeroClimber(ClimberSubsystem climberSubsystem, double windAmount) {
-  //   return 
-  //    new InstantCommand( 
-  //      ()-> {
-  //        climberSubsystem.zeroEncoder();
-  //        //System.out.println("Zero");
-  //       } )
-  //    ;
-   
-  // }
 }
