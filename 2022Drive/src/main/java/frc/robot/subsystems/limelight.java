e7d92db8b998c6ebef09b3fc052f68b662a40d3a
Sam <Srabbjaros@gmail.com>
2023-01-21 12:07:36 -0500

    limelight distace update

--- 2022Drive/src/main/java/frc/robot/subsystems/limelight.java
+++ 2022Drive/src/main/java/frc/robot/subsystems/limelight.java
@@ -5,24 +5,22 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import java.lang.Math;
-public class limelight {
-    
-public static void test(){
+public class Limelight {
+// how many degrees back is your limelight rotated from perfectly vertical?
+   private static final double limelightMountAngleDegrees = 0;
+  // distance  from the center of the Limelight lens to the floor
+  private static final  double limelightLensHeightInches = 30.0; 
+  // distance from the target to the floor
+  private static final double goalHeightInches = 63.0;
+// distance from the target to the floor
+ private static final  double goalHeightInches = 63.0;
+
+public static double test(){
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry ty = table.getEntry("ty");
     
     double targetOffsetAngle_Vertical = ty.getDouble(0.0);
-    
-    // how many degrees back is your limelight rotated from perfectly vertical?
-    double limelightMountAngleDegrees = 0
-    ;
-    
-    // distance from the center of the Limelight lens to the floor
-    double limelightLensHeightInches = 30.0;
-    
-    // distance from the target to the floor
-    double goalHeightInches = 59.0;
     
     double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
     double angleToGoalRadians = angleToGoalDegrees * (Math.PI / 180.0);
@@ -31,6 +29,6 @@ public static void test(){
     double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
     System.out.println(distanceFromLimelightToGoalInches + "   " +angleToGoalDegrees  );
 
-//return distanceFromLimelightToGoalInches;
+        return distanceFromLimelightToGoalInches;
     }
 }
