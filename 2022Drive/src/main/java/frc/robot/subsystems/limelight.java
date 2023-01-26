2591c5619c95d52b8a358cce68d8b72507860409
Sam <Srabbjaros@gmail.com>
2023-01-26 16:45:09 -0500

    angle offset x

--- 2022Drive/src/main/java/frc/robot/subsystems/limelight.java
+++ 2022Drive/src/main/java/frc/robot/subsystems/limelight.java
@@ -7,27 +7,35 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import java.lang.Math;
 public class Limelight {
 // how many degrees back is your limelight rotated from perfectly vertical?
-   private static final double limelightMountAngleDegrees = 0;
+    private static final double limelightMountAngleDegrees = 0;
   // distance  from the center of the Limelight lens to the floor
-  private static final  double limelightLensHeightInches = 29.0; 
+    private static final  double limelightLensHeightInches = 29.0; 
   // distance from the target to the floor
-  private static final double goalHeightInches = 63;
+    private static final double goalHeightInches = 63;
 
 
-public  double getDistance(){
+    public double getDistance() {
 
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry ty = table.getEntry("ty");
-    
-    double targetOffsetAngle_Vertical = ty.getDouble(0.0);
-    
-    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-    double angleToGoalRadians = angleToGoalDegrees * (Math.PI / 180.0);
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry ty = table.getEntry("ty");
+        
+        double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+        
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+        double angleToGoalRadians = angleToGoalDegrees * (Math.PI / 180.0);
 
-    //calculate distance
-    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
-    System.out.println(distanceFromLimelightToGoalInches );
+        //calculate distance
+        double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
+        System.out.println(distanceFromLimelightToGoalInches);
 
         return distanceFromLimelightToGoalInches;
+    }
+    public double getXOffset() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        
+        double targetOffsetAngle_Horizontal = tx.getDouble(0.0);
+
+        return targetOffsetAngle_Horizontal;
     }
 }
