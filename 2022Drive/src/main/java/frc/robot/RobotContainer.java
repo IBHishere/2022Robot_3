1a2b94ef0743d646d55d83c1b7c0541d49b6ad27
Sam <Srabbjaros@gmail.com>
2023-01-28 12:10:44 -0500

    l

--- 2022Drive/src/main/java/frc/robot/RobotContainer.java
+++ 2022Drive/src/main/java/frc/robot/RobotContainer.java
@@ -42,13 +42,13 @@ public class RobotContainer {
   
 XboxController  m_driveController = new XboxController(Constants.DRIVE_XBOX_CONTROLLER);
 XboxController  m_helperController = new XboxController(Constants.HELPER_XBOX_CONTROLLER);
-Limelight limelight = new Limelight();
+
 
 // The robot's subsystems and commands are defined here...
 private final MecanumDriveSubsystem m_MecanumDriveSubsystem = new MecanumDriveSubsystem();
- // private final DriveTrainSubsystem driveSubsystem = new DriveTrainSubsystem();
-  private final Limelight test = new Limelight();
-  
+ // private final DriveTrainSubsystem driveSubsystem = new DriveTrainSubsystem();;
+  private final GrabberSubsystem Grabber = new GrabberSubsystem();
+  private final Limelight limelight = new Limelight();
   private edu.wpi.first.wpilibj2.command.button.Button whenPressed;
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   
